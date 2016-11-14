06a0c98d5855cbbc3b9c04be5c786c9afbbde848
David J <david.j.361@gmail.com>
2016-11-14 15:31:14 -0500

    Finished setting things up for working off the generated file.

--- Overseer/test.py
+++ Overseer/test.py
@@ -3,20 +3,28 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from UImainwindow import Ui_MainWindow
 
-# This is our code that is working off the generated code from pycui5
+# This is our code that is working off the generated code from pyuic5
 class OverseerMainWindow(Ui_MainWindow):
     def __init__(self):
         super(OverseerMainWindow, self).__init__()
+        self.app = QApplication(sys.argv)
+        self.MainWindow = QMainWindow()
+        self.setupUi(self.MainWindow)
+        self.setupProcessesList()
 
-        # Add local changes below
+    def show():
+        self.MainWindow.show()
+
+    # Add local changes below
+    def setupProcessesList(self):
+        # I have no idea how to get update the model
+        # Even if columnview is not the right class, we need to figure this out
+        self.columnView.setModel(self, QtCore.QAbstractItemModel(["test1","test2"]))
+
 
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    MainWindow = QMainWindow()
     ui = OverseerMainWindow()
-    ui.setupUi(MainWindow)
-
-    MainWindow.show()
+    ui.show()
     sys.exit(app.exec_())
