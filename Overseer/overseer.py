1c0801ca1e84170044183322e7d0b81d3c521819
Trent McNabb <Trent McNabb>
2016-11-23 11:17:15 -0500

    merged test.py and overseer.py

--- Overseer/overseer.py
+++ Overseer/overseer.py
@@ -28,11 +28,30 @@ class OverseerMainWindow(Ui_MainWindow):
         self.model.setHeaderData(5, 1, "Description")
         self.tableView.setModel(self.model)
 
-        for i in range(9):
-            for j in range(7):
-                string = str(i) + "," + str(j)
-                item = QtGui.QStandardItem(string)
-                self.model.setItem(i,j, item)
+        p = subprocess.Popen(["ls", "/proc"], stdout=subprocess.PIPE, shell=False, universal_newlines=True)
+        (output, err) = p.communicate()
+        arr = output.split('\n')
+        outArr = [];
+        for element in arr:
+            if(element.isdigit()):
+                cmdString = "/proc/" + element + "/status"
+                p2 = subprocess.Popen(["cat", cmdString], stdout=subprocess.PIPE, shell=False, universal_newlines=True)
+                (output2, err) = p2.communicate()
+                arr2 = output2.split('\n')
+                for element2 in arr2:
+                    if((element2) and (element2[0] == 'N') and (element2[1] == 'a')):
+                        element = element.strip(" ")
+                        element2 = element2.strip("Name:")
+                        element2 = element2.strip(" ")
+                        element2 = element2.strip('\t')
+                        outArr.append([element, element2])
+        i = 0
+        for element in outArr:
+            item = QtGui.QStandardItem0i(element[1])
+            self.model.setItem(i,0, item)
+            item = QtGUi.QStandardItem(element[0])
+            self.model.setItem(i,1, item)
+            i++
         self.tableView.setModel(self.model)
         self.tableView.verticalHeader().setVisible(False)
 
