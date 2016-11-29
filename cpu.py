08ef749d22796f5e0780a1ff1b6375e39d085164
David J <david.j.361@gmail.com>
2016-11-29 16:57:35 -0500

    Added CPU class. Added ability to read in CPU times.

---
+++ cpu.py
@@ -0,0 +1,22 @@
+class CPU:
+    # Note: Guest and Guest_nice are already accounted in user and nice, hence they are not included in the total calculation
+    def __init__(self):
+        self.usertime = 0 # user   (1) Time spent in user mode.
+        self.nicetime = 0 # nice   (2) Time spent in user mode with low priority (nice).
+        self.systime = 0 # system (3) Time spent in system mode.
+        self.idletime = 0 # idle   (4) Time spent in the idle task.  This value should be USER_HZ times the second entry in the /proc/uptime pseudo-file.
+        self.iotime = 0 # iowait (since Linux 2.5.41) (5) Time waiting for I/O to complete.
+        self.irqtime = 0 # irq (since Linux 2.6.0-test4) (6) Time servicing interrupts.
+        self.softirqtime = 0 # softirq (since Linux 2.6.0-test4) (7) Time servicing softirqs.
+        self.stealtime = 0 # steal (since Linux 2.6.11) (8) Stolen time, which is the time spent in other operating systems when running in a virtualized environment
+        self.totaltime = 0
+    def __init__(self, usertime, nicetime, systime, idletime, iotime, irqtime, softirqtime, stealtime):
+        self.usertime = usertime # user   (1) Time spent in user mode.
+        self.nicetime = nicetime # nice   (2) Time spent in user mode with low priority (nice).
+        self.systime = systime # system (3) Time spent in system mode.
+        self.idletime = idletime # idle   (4) Time spent in the idle task.  This value should be USER_HZ times the second entry in the /proc/uptime pseudo-file.
+        self.iotime = iotime # iowait (since Linux 2.5.41) (5) Time waiting for I/O to complete.
+        self.irqtime = irqtime # irq (since Linux 2.6.0-test4) (6) Time servicing interrupts.
+        self.softirqtime = softirqtime # softirq (since Linux 2.6.0-test4) (7) Time servicing softirqs.
+        self.stealtime = stealtime # steal (since Linux 2.6.11) (8) Stolen time, which is the time spent in other operating systems when running in a virtualized environment
+        self.totaltime = self.usertime + self.nicetime + self.systime + self.idletime + self.iotime + self.irqtime + self.softirqtime + self.softirqtime + self.stealtime
--- proc.py
+++ proc.py
@@ -2,17 +2,20 @@ import os
 import sys
 import re
 from process import Process
+from cpu import CPU
 
 class Proc:
     def __init__(self):
         self.proc = '/proc/'
         self.processList = []
+        self.cpu = [] # cpu[0] should be the collective info on all CPUs
         self.totalMem = 0
         # Will hold our pids that are inside /proc/ as well as the process's information
         self.readData()
 
     def readData(self):
         self.readTotalMem()
+        self.readcpuTimes()
         self.readProcListData()
 
     # FIXME: skip looping and try to acess directly
@@ -20,10 +23,25 @@ class Proc:
         fd = open(self.proc + "/meminfo")
         for i, line in enumerate(fd):
             if i == 0:
-                items = line.split(" ")
+                # items = line.split(" ") # OLD
+                items = re.split("[\t ]+", line)
                 for j, item in enumerate(items):
                     if items[j].isdigit():
                         self.totalMem = int(items[j])
+            else:
+                break
+        fd.close()
+        
+    # FIXME: skip looping and try to acess directly
+    def readcpuTimes(self):
+        fd = open(self.proc + "/stat")
+        self.cpu = []
+        for i, line in enumerate(fd):
+            # What it looks like: cpu  67188 150 8766 5124308 14884 0 711 0 0 0
+            if i < 5: # Our collective CPU data
+                items = re.split("[\t ]+", line)
+                cpu = CPU(items[1],items[2],items[3],items[4],items[5],items[6],items[7],items[8])
+                self.cpu.append(cpu)
             else:
                 break
         fd.close()
@@ -44,7 +62,8 @@ class Proc:
             process.stime = ""
             process.ramPrecentage = 0
             for i, line in enumerate(fd):
-                item = line.split(" ")
+                # item = line.split(" ") # OLD
+                item = re.split("[\t ]+", line)
                 process.name = item[1][1:-1]
 
                 process.rss = item[23]
@@ -58,3 +77,11 @@ class Proc:
                 process.stime = item[16]
             fd.close()
             self.processList.append(process)
+
+
+# FIXME: Delete when done project
+# For testing proc.py's data gathering
+if __name__ == "__main__":
+    proc = Proc()
+    for iter in proc.cpu:
+        print(iter.totaltime)
